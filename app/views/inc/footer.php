4f9c061c4857a0cc962eb0cd6d83810954538b5d
gagelasher <rsgage21@gmail.com>
2022-07-28 14:22:26 -0600

    commit

---
+++ app/views/inc/footer.php
@@ -0,0 +1,8 @@
+
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js
+"></script>
+<script src="<?= URLROOT;?>/js/main.js"></script>
+</body>
+</html>
---
+++ app/views/pages/index.php
@@ -0,0 +1,14 @@
+<?php
+require APPROOT . '/views/inc/header.php';
+require APPROOT . '/views/inc/footer.php';
+
+
+
+?>
+
+<div class="m-5 p-5 bg-dark text-white rounded text-center">
+   <div class="container">
+    <h1 class="display-3"><?= $data['title']; ?></h1>
+    <p class="lead">Simple social network built on the TraversyMVC PHP framework </p>
+   </div> 
+</div>
